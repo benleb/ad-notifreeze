768b76dabfb47b3d3608cc3459701af6df132270
Ben Lebherz <git@benleb.de>
2019-10-01 14:48:46 +0200

    Bump version: 0.4.2 → 0.4.3

--- apps/notifreeze/notifreeze.py
+++ apps/notifreeze/notifreeze.py
@@ -17,7 +17,7 @@ import hassapi as hass
 
 APP_NAME = "NotiFreeze"
 APP_ICON = "❄️ "
-APP_VERSION = "0.4.2"
+APP_VERSION = "0.4.3"
 
 # state set by home assistant if entity exists but no state
 STATE_UNKNOWN = "unknown"
